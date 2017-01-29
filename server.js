0e7e121717996ee76ed2ef66409097900b258e58
Will G <will.gutierrez@gmail.com>
2017-01-29 06:26:20 +0000

    counters database debugging

--- server.js
+++ server.js
@@ -118,10 +118,15 @@ app.get('/debug', function(request, response) {
 
     var counters = database.collection('counters');
 
-    counters.insertOne({ name: 'url_count', seq: 1 }).then(function(boolean) {
-      response.send(boolean);
+    // counters.insertOne({ name: 'url_count', seq: 1 }).then(function(boolean) {
+    //   response.send(boolean);
+    // });
+
+    counters.find({}).each(function(error, document)) {
+      if (error) throw error;
+
+      console.log(document);  
     });
-  });
 
 });
 
